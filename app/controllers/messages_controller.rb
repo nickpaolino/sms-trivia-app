d607e223b48c55f97a577f583b4719d02953ff63
Nick Paolino <github email address>
2017-11-22 22:12:37 -0500

    Updated number

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,12 @@
 class MessagesController < ApplicationController
 
   def reply
-    account_sid = ""
-    auth_token = ""
+
     @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
     message_body = params["Body"]
     from_number = params["From"]
     sms = @client.messages.create(
-      from: "+1646791080",
+      from: "+16467913080",
       to: from_number,
       body: "Hello #{from_number}, thanks for texting me."
     )
