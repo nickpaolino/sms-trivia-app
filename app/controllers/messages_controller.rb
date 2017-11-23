34fb8169cf73b80b078719c50f2f1b66af7c2514
Nick Paolino <github email address>
2017-11-22 22:29:16 -0500

    Added message model and show action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,28 @@
 class MessagesController < ApplicationController
 
   def reply
-
     @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
     message_body = params["Body"]
     from_number = params["From"]
+
+    create_record(message_body, from_number)
+
     sms = @client.messages.create(
       from: "+16467913080",
       to: from_number,
       body: "Hello #{from_number}, thanks for texting me."
     )
+  end
+
+  def show
+    @messages = Message.all
+
+    render json: @messages
+  end
+
+  private
+
+  def create_record(message_body, from_number)
+    Message.create(content: message_body, number: from_number)
   end
 end
