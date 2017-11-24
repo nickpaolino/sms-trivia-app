af30653ba8c77ad5164244b79e5b9b2931164807
Nick Paolino <github email address>
2017-11-24 17:00:57 -0500

    Updated phone_number from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20171123031736) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
